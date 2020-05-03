cdc747cadde98c246dd505f5b3a312aeb1088e84
seenu-g <srinivasan.gsvasan@gmail.com>
2020-05-03 14:14:34 +0530

    modify test and train

--- layers.py
+++ layers.py
@@ -32,12 +32,13 @@ class BilinearUpSampling2D(Layer):
             height = self.size[0] * input_shape[2] if input_shape[2] is not None else None
             width = self.size[1] * input_shape[3] if input_shape[3] is not None else None
         elif self.data_format == 'channels_last':
-            height = self.size[0] * input_shape[1] if input_shape[1] is not None else None
+            height = self.size[0] * input_shape[1] if input_shape[1] is not None else Non
             width = self.size[1] * input_shape[2] if input_shape[2] is not None else None
         
-        return tf.image.resize_images(inputs, [height, width], method=tf.image.ResizeMethod.BILINEAR, align_corners=True)
+        return tf.image.resize(inputs, [height, width], method=tf.image.ResizeMethod.BILINEAR, antialias = True)
 
     def get_config(self):
         config = {'size': self.size, 'data_format': self.data_format}
         base_config = super(BilinearUpSampling2D, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
+    
